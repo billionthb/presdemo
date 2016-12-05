3b6b58d1438724e259e14eb864a9fb0320c4ae41
William Steele <billionthb@yahoo.com>
2016-12-05 14:02:42 -0500

    Added demo.go

---
+++ demo.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	
+	"github.com/billionthb/stringutil"
+)
+
+func main() {
+	fmt.Printf(stringutil.Reverse("This is a demonstration of the 'go get' utility, but backwards"))
+}
